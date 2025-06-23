356fe3f47ad6f439605eb20416b2224c21f20e2c
Evan <evanaditya18@gmail.com>
2025-06-23 14:49:21 +0700

    .

--- backend/hello.go
+++ backend/hello.go
@@ -19,12 +19,20 @@ import (
 )
 
 // Database connection details
+// const (
+// 	user     = "postgres.jdujcnoojlthzezlgebw"
+// 	password = "Kx6rdWWKpSztKN3P"
+// 	host     = "aws-0-ap-southeast-1.pooler.supabase.com"
+// 	port     = 5432
+// 	dbName   = "postgres"
+// )
+
 const (
-	user     = "postgres.jdujcnoojlthzezlgebw"
-	password = "Kx6rdWWKpSztKN3P"
-	host     = "aws-0-ap-southeast-1.pooler.supabase.com"
+	user     = "postgres"
+	password = "12345678"
+	host     = "localhost"
 	port     = 5432
-	dbName   = "postgres"
+	dbName   = "gudang_garam"
 )
 
 // User represents a user for authentication purposes.
@@ -126,7 +134,8 @@ func main() {
 
 // openDB initializes and returns a new PostgreSQL database connection.
 func openDB() *sql.DB {
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbName)
+	// psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbName)
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
 		fmt.Println("Error opening database: ", err)
